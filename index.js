92b58028509e8a07fe48956564c34b42dae9a255
Deseteral <deseteral@gmail.com>
2018-10-11 19:38:50 +0200

    Renamed leadingZeros option to leading

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var parseMs = require('parse-ms')
 var addZero = require('add-zero')
 
 module.exports = function (ms, options) {
-  var leadingZeros = options && options.leadingZeros
+  var leading = options && options.leading
   var unsignedMs = ms < 0 ? -ms : ms
   var sign = ms <= -1000 ? '-' : ''
   var t = parseMs(unsignedMs)
   var seconds = addZero(t.seconds)
   if (t.days) return sign + t.days + ':' + addZero(t.hours) + ':' + addZero(t.minutes) + ':' + seconds
-  if (t.hours) return sign + (leadingZeros ? addZero(t.hours) : t.hours) + ':' + addZero(t.minutes) + ':' + seconds
-  return sign + (leadingZeros ? addZero(t.minutes) : t.minutes) + ':' + seconds
+  if (t.hours) return sign + (leading ? addZero(t.hours) : t.hours) + ':' + addZero(t.minutes) + ':' + seconds
+  return sign + (leading ? addZero(t.minutes) : t.minutes) + ':' + seconds
 }
